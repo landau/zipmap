4bd4d9f42a0b2967bf3c1277b8a7f7b99cb752c0
Trevor Landau <landautrevor@gmail.com>
2014-11-14 15:19:47 -0500

    add arity one handling

--- index.js
+++ index.js
@@ -4,19 +4,51 @@ function nth(arr, idx) {
   return arr[idx];
 }
 
-/**
- * Returns a map with the keys mapped to the corresponding vals.
- *
- * @param {array} keys
- * @param {array} vals
- *
- * @return {object}
- */
-module.exports = function zipmap(keys, vals) {
+var toString = Function.call.bind(Object.prototype.toString);
+
+function isObj(o) {
+  return toString(o) === '[object Object]';
+}
+
+// Original zipmap
+function _zipmap(keys, vals) {
   var shorter = keys.length > vals.length ? vals : keys;
 
   return shorter.reduce(function(map, val, idx) {
     map[nth(keys, idx)] = nth(vals, idx);
     return map;
   }, {});
+
+}
+
+function zipmapPairs(pairs) {
+  return pairs.reduce(function(map, pair) {
+    map[nth(pair, 0)] = nth(pair, 1);
+    return map;
+  }, {});
+}
+
+function zipmapObj(objs) {
+  return objs.reduce(function(map, o) {
+    map[o.key] = o.value;
+    return map;
+  }, {});
+}
+
+/**
+ * Returns a map with the keys mapped to the corresponding vals.
+ *
+ * @param {array} keys
+ * @param {array} [vals]
+ *
+ * @return {object}
+ */
+module.exports = function zipmap(keys, vals) {
+  if (!vals) {
+    if (Array.isArray(nth(keys, 0))) return zipmapPairs(keys);
+    if (isObj(nth(keys, 0))) return zipmapObj(keys);
+    throw new TypeError('Expected vals to be an array');
+  }
+
+  return _zipmap(keys, vals);
 };
